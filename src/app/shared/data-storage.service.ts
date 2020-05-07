ca06eb07ee1ed21fc0c9d16e5555cf2720823744
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-05-07 23:08:06 +0200

    Fetching Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
+import {Recipe} from "../recipes/recipe.model";
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,11 @@ export class DataStorageService {
       });
   }
 
-
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>('https://ngcg-recipe-book.firebaseio.com/recipes.json')
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+  }
 }
