0455fdb1b7323125bfa1c2e2c89abb7355abd4b6
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-05-09 01:30:08 +0200

    Understanding Shared Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,20 @@ import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 
-import {DropdownDirective} from "./shared/dropdown.directive";
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {AuthComponent} from "./auth/auth.component";
-import {LoadingSpinnerComponent} from "./shared/loading-spinner/loading-spinner.component";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-import {AlertComponent} from "./shared/alert/alert.component";
-import {PlaceholderDirective} from "./shared/placeholder/placeholder.directive";
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
     AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -32,7 +25,8 @@ import {ShoppingListModule} from "./shopping-list/shopping-list.module";
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
-    ShoppingListModule
+    ShoppingListModule,
+    SharedModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
